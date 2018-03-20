289edcf68783be813568a94d1fbb7342f1da69f1
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-03-20 13:01:36 +0100

    add_tasks_list_comment

--- js/index.jsx
+++ js/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx'
 
+//Tasks:
+// - remove all api calls => to reducers
+// - implement new api calls
+// - add shoping list option (to print for each recipe)
+// - add better names for components and clean up the code
+
 document.addEventListener('DOMContentLoaded', function () {
     ReactDOM.render(
         <App/>,
